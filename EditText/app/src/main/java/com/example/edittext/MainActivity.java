3aade22a2986a0670e2d1c993fb5d52c6c459457
wonhyuna <hyunawon417@gmail.com>
2023-01-02 23:10:35 +0900

    안드로이드 자바 입문 강의 #1, #2 완료

---
+++ EditText/app/src/main/java/com/example/edittext/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.edittext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et_id;
+    Button btn_test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //변수 설정정
+       et_id = findViewById(R.id.et_id);
+        btn_test = findViewById(R.id.btn_test);
+
+        //버튼을 클릭했을 때, edit text에 안드로이드라는 글씨를 써 줌
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                et_id.setText("안드로이드");
+            }
+        });
+    }
+}
