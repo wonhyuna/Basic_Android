50c79eec33cee6a749c8102ab187ab14ff98f05a
wonhyuna <hyunawon417@gmail.com>
2023-01-04 21:37:37 +0900

    안드로이드 자바 입문 강의 #8 완료

---
+++ SharedExample/app/src/main/java/com/example/sharedexample/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.sharedexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et_save;
+    String shared = "file";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_save = (EditText) findViewById(R.id.et_save);
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+
+        //처음 빈 값으로 초기화
+        String value = sharedPreferences.getString("android", "");
+        et_save.setText(value);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String value = et_save.getText().toString();
+        editor.putString("android", value);
+        editor.commit();
+    }
+}
