fe0b1cef54048d9c4a6b91f04a959508066005a5
wonhyuna <hyunawon417@gmail.com>
2023-01-07 17:44:04 +0900

    안드로이드 자바 입문 강의 #20 완료

---
+++ SpinnerExample/app/src/main/java/com/example/spinnerexample/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.spinnerexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private TextView tv_result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        tv_result = (TextView) findViewById(R.id.tv_result);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                tv_result.setText(adapterView.getItemAtPosition(i).toString()); //선택한 값을 textview에 보이게 함
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
