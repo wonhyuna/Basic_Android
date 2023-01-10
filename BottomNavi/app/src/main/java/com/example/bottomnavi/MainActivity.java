42865438b386188cdc6e43c5e700f2d362164ef0
wonhyuna <hyunawon417@gmail.com>
2023-01-10 22:58:00 +0900

    안드로이드 자바 입문 강의 #25 완료

---
+++ BottomNavi/app/src/main/java/com/example/bottomnavi/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.bottomnavi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Frag1 frag1;
+    private Frag2 frag2;
+    private Frag3 frag3;
+    private Frag4 frag4;
+    private Frag5 frag5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_bright:
+                        setFrag(0);
+                        break;
+                    case R.id.action_favorite:
+                        setFrag(1);
+                        break;
+                    case R.id.action_restore:
+                        setFrag(2);
+                        break;
+                    case R.id.action_rss:
+                        setFrag(3);
+                        break;
+                    case R.id.action_school:
+                        setFrag(4);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+        frag4 = new Frag4();
+        frag5 = new Frag5();
+        setFrag(0); //첫 프래그먼트 지정
+    }
+
+    //Fragment 교체
+    private void setFrag(int n){
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch(n){
+            case 0:
+                ft.replace(R.id.main_frame, frag1);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, frag2);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, frag3);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame, frag4);
+                ft.commit();
+                break;
+            case 4:
+                ft.replace(R.id.main_frame, frag5);
+                ft.commit();
+                break;
+        }
+    }
+}
